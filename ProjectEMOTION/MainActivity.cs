4ce9a07ae434c38a68d66da7272e6a8d0689d3dd
tnagy <tnagy@TREVOR-THINKPAD>
2016-12-28 15:08:09 -0500

    Add project files.

---
+++ ProjectEMOTION/MainActivity.cs
@@ -0,0 +1,87 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Plugin.Media;
+using System;
+
+namespace ProjectEMOTION
+{
+    [Activity(Label = "ProjectEMOTION", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Button _btnTakePhoto;
+        private Button _btnChoosePhoto;
+        private Button _btnTutorial;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+            _btnTakePhoto = FindViewById<Button>(Resource.Id.btnTakePhoto);
+            _btnChoosePhoto = FindViewById<Button>(Resource.Id.btnChoosePhoto);
+            _btnTutorial = FindViewById<Button>(Resource.Id.btnTutorial);
+
+            _btnTakePhoto.Click += _btnTakePhoto_Click;
+            _btnChoosePhoto.Click += _btnChoosePhoto_Click;
+            _btnTutorial.Click += _btnTutorial_Click;
+        }
+
+        private void _btnTutorial_Click(object sender, System.EventArgs e)
+        {
+            // New page
+            throw new System.NotImplementedException();
+        }
+
+        private async void _btnChoosePhoto_Click(object sender, System.EventArgs e)
+        {
+            // Choose from gallery
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                Console.WriteLine("No Gallery", ":( No gallery available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync();
+
+            if (file == null)
+                return;
+
+            Console.WriteLine(file.Path);
+        }
+
+        private async void _btnTakePhoto_Click(object sender, System.EventArgs e)
+        {
+            // Launch camera
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                Console.WriteLine("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "temp",
+                Name = "facetotest.jpg",
+                SaveToAlbum = false,
+            });
+
+            if (file == null)
+                return;
+
+            Console.WriteLine(file.Path);
+
+            file.Dispose();
+
+            // After this call function that opens new page and submits it to the microsoft api
+
+        }
+    }
+            
+}
+
